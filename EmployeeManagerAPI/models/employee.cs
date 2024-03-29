dac64eb20c4280e7668207a740878de53529f90e
Yjoubayr <YossefJMT@gmail.com>
2024-03-29 15:44:05 +0100

    Create employee.cs

---
+++ EmployeeManagerAPI/models/employee.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public string SSN { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(1)]
+        public string MiddleInitial { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+        
+        public string Sex { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string FullName => $"{FirstName} {MiddleInitial} {LastName}";
+
+    }
+}

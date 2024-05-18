501c6e211cf82c19785e0b1d36332b9afb56b8ae
Yjoubayr <YossefJMT@gmail.com>
2024-05-18 17:26:19 +0200

    Works on service

---
+++ EmployeeManagerAPI/Controllers/Services/WorksOnService.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagerAPI.Data;
+using EmployeeManagerAPI.Models;
+
+namespace EmployeeManagerAPI.Services
+{
+    public class WorksOnService(DataContext context)
+    {
+        private readonly DataContext _context = context ?? throw new ArgumentNullException(nameof(context));
+
+        public async Task<IEnumerable<WorksOn>> GetWorksOnsAsync()
+        {
+            return await _context.WorksOns.ToListAsync();
+        }
+
+        public async Task<WorksOn?> GetWorksOnAsync(string employeeSSN, string projectName, int projectNumber)
+        {
+            return await _context.WorksOns.FindAsync(employeeSSN, projectName, projectNumber);
+        }
+
+        public async Task UpdateWorksOnAsync(string employeeSSN, string projectName, int projectNumber, WorksOn worksOn)
+        {
+            if (employeeSSN != worksOn.EmployeeSSN || projectName != worksOn.ProjectName || projectNumber != worksOn.ProjectNumber)
+            {
+                throw new ArgumentException("WorksOn IDs mismatch");
+            }
+            _context.Entry(worksOn).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddWorksOnAsync(WorksOn worksOn)
+        {
+            _context.WorksOns.Add(worksOn);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteWorksOnAsync(string employeeSSN, string projectName, int projectNumber)
+        {
+            var worksOn = await _context.WorksOns.FindAsync(employeeSSN, projectName, projectNumber) ?? throw new ArgumentException("WorksOn not found");
+            _context.WorksOns.Remove(worksOn);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- EmployeeManagerAPI/Controllers/WorksOnsController.cs
+++ EmployeeManagerAPI/Controllers/WorksOnsController.cs
@@ -1,125 +1,77 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using EmployeeManagerAPI.Data;
 using EmployeeManagerAPI.Models;
+using EmployeeManagerAPI.Services;
 
 namespace EmployeeManagerAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class WorksOnsController : ControllerBase
+    public class WorksOnsController(WorksOnService worksOnService) : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly WorksOnService _worksOnService = worksOnService ?? throw new ArgumentNullException(nameof(worksOnService));
 
-        public WorksOnsController(DataContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/WorksOns
         [HttpGet]
         public async Task<ActionResult<IEnumerable<WorksOn>>> GetWorksOns()
         {
-            return await _context.WorksOns.ToListAsync();
+            var worksOns = await _worksOnService.GetWorksOnsAsync();
+            return Ok(worksOns);
         }
 
-        // GET: api/WorksOns/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<WorksOn>> GetWorksOn(string id)
+        [HttpGet("{employeeSSN}/{projectName}/{projectNumber}")]
+        public async Task<ActionResult<WorksOn>> GetWorksOn(string employeeSSN, string projectName, int projectNumber)
         {
-            var worksOn = await _context.WorksOns.FindAsync(id);
-
+            var worksOn = await _worksOnService.GetWorksOnAsync(employeeSSN, projectName, projectNumber);
             if (worksOn == null)
             {
                 return NotFound();
             }
-
-            return worksOn;
+            return Ok(worksOn);
         }
 
-        // PUT: api/WorksOns/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutWorksOn(string id, WorksOn worksOn)
+        [HttpPut("{employeeSSN}/{projectName}/{projectNumber}")]
+        public async Task<IActionResult> PutWorksOn(string employeeSSN, string projectName, int projectNumber, WorksOn worksOn)
         {
-            if (id != worksOn.EmployeeSSN)
+            try
+            {
+                await _worksOnService.UpdateWorksOnAsync(employeeSSN, projectName, projectNumber, worksOn);
+                return NoContent();
+            }
+            catch (ArgumentException)
             {
                 return BadRequest();
             }
-
-            _context.Entry(worksOn).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!WorksOnExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        // POST: api/WorksOns
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<WorksOn>> PostWorksOn(WorksOn worksOn)
         {
-            worksOn.Employee ??= null;
-            worksOn.Project ??= null;
-
-            _context.WorksOns.Add(worksOn);
             try
             {
-                await _context.SaveChangesAsync();
+                await _worksOnService.AddWorksOnAsync(worksOn);
+                return CreatedAtAction(nameof(GetWorksOn), new { employeeSSN = worksOn.EmployeeSSN, projectName = worksOn.ProjectName, projectNumber = worksOn.ProjectNumber }, worksOn);
             }
-            catch (DbUpdateException)
+            catch (ArgumentException)
             {
-                if (WorksOnExists(worksOn.EmployeeSSN))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+                return Conflict();
             }
-
-            return CreatedAtAction("GetWorksOn", new { id = worksOn.EmployeeSSN }, worksOn);
         }
 
-        // DELETE: api/WorksOns/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteWorksOn(string id)
+        [HttpDelete("{employeeSSN}/{projectName}/{projectNumber}")]
+        public async Task<IActionResult> DeleteWorksOn(string employeeSSN, string projectName, int projectNumber)
         {
-            var worksOn = await _context.WorksOns.FindAsync(id);
-            if (worksOn == null)
+            try
+            {
+                await _worksOnService.DeleteWorksOnAsync(employeeSSN, projectName, projectNumber);
+                return NoContent();
+            }
+            catch (ArgumentException)
             {
                 return NotFound();
             }
-
-            _context.WorksOns.Remove(worksOn);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool WorksOnExists(string id)
-        {
-            return _context.WorksOns.Any(e => e.EmployeeSSN == id);
         }
     }
 }
